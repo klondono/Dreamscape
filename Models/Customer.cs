1c5a9bcfcfd8f623c52249fa7796acacd32269d0
klondono <klondono1016@gmail.com>
2020-06-03 13:59:35 -0400

    Check in

---
+++ Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dreamscape.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public bool ClubMember { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime TimeUpdated { get; set; }
+
+        public int CreatedBy { get; set; }
+
+    }
+}
