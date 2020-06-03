eddb54ae59a0f88541c3eb0856be2a1dfee23e6c
Samuel Borja <sb.pdc.ny@gmail.com>
2020-06-03 14:55:11 -0400

    added Customer.Controller

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dreamscape.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dreamscape.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: CustomerController
+        public ActionResult Index()
+        {
+            var customerList = new List<Customer>();
+
+            var customer1 = new Customer();
+            var customer2 = new Customer();
+
+            customer1.FirstName = "Kerwin";
+            customer1.LastName = "Londono";
+
+            customer2.FirstName = "Sam";
+            customer2.LastName = "Borja";
+
+            customerList.Add(customer1);
+            customerList.Add(customer2);
+
+            return View(customerList);
+        }
+
+        // GET: CustomerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Threading.Tasks;
+
+namespace Dreamscape.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateHired { get; set; }
+
+        public DateTime DOB { get; set; }
+        public string Department { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime TimeUpdated { get; set; }
+
+        public int CreatedBy { get; set; }
+
+
+    }
+}
